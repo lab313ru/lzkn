c84e9b1bcb6f9b657f963fdb25dc7b04b224b535
Hans Bonini <hans@ysolutions.com.br>
2021-09-14 15:46:14 -0300

    Added Decompressors for LZKN SNES version variants 1 and 2

---
+++ lzkn_snes_2/main.h
@@ -0,0 +1,28 @@
+#ifndef SIM__HEADER
+#define SIM__HEADER
+
+#ifdef _WIN32
+
+  /* You should define ADD_EXPORTS *only* when building the DLL. */
+  #ifdef ADD_EXPORTS
+    #define ADDAPI __declspec(dllexport)
+  #else
+    #define ADDAPI __declspec(dllimport)
+  #endif
+
+  /* Define calling convention in one place, for convenience. */
+  #define ADDCALL __cdecl
+
+#else /* _WIN32 not defined. */
+
+  /* Define with no value on non-Windows OSes. */
+  #define ADDAPI
+  #define ADDCALL
+
+#endif
+
+// ADDAPI int ADDCALL decompress(unsigned char *input, unsigned char *output);
+// ADDAPI int ADDCALL compress(unsigned char *input, unsigned char *output, int size);
+// ADDAPI int ADDCALL compressed_size(unsigned char *input);
+
+#endif /* SIM__HEADER */
